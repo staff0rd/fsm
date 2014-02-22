9d00c3c2d0fdc26012992f5a690b1a7ec86dbae5
stafford <staff0rd@users.noreply.github.com>
2014-02-22 21:36:18 +1100

    added folder structure for rules

---
+++ Fsm.DataScraper/Services/ScraperRules/Chapter/FirstChapterTwelve.cs
@@ -0,0 +1,17 @@
+﻿using Fsm.DataScraper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fsm.DataScraper.Services.ScraperRules
+{
+    public class FirstChapterTwelve : ScraperRule, IFirstChapterRule
+    {
+        public Chapter GetChapter()
+        {
+            return new Chapter { Number = 12 };
+        }
+    }
+}
---
+++ Fsm.DataScraper/Services/ScraperRules/Verse/VerseOffsetBumpByFive.cs
@@ -0,0 +1,17 @@
+﻿using Fsm.DataScraper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fsm.DataScraper.Services.ScraperRules
+{
+    public class VerseOffsetBumpByFive : ScraperRule, IVerseOffsetRule
+    {
+        public int GetVerseOffset(Chapter chapter)
+        {
+            return chapter.Verses.Count + 5;
+        }
+    }
+}
